0b991570988c2b71c00a66f77eb57cce527cc4b7
Ved panchal <128056477+Ved-panchal@users.noreply.github.com>
2024-02-03 19:53:02 +0530

    Update PaymentControllers.js

--- server/controllers/PaymentControllers.js
+++ server/controllers/PaymentControllers.js
@@ -2,29 +2,26 @@ import { instance } from '../server.js'
 import crypto from 'crypto'
 
 
+var count=1;
+
 export const checkout = async (req, res) => {
-    try{
-        const options = {
-            amount: Number(req.body.amount * 100),
-            currency: 'INR',
-        }
-        const order = await instance.orders.create(options)
-        res.status(200).json({
-            success: true,
-            order,
-        })
-    } catch(err){
-        res.status(404).json({
-            success:false,
-            error:err.message
-        })
+    const options = {
+        amount: Number(req.body.amount * 100),
+        currency: 'INR',
     }
+    const order = await instance.orders.create(options)
+
+
+    res.status(200).json({
+        success: true,
+        order,
+    })
 }
 
 export const paymentVerification = async (req, res) => {
-    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
-        req.body
-
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body
+    
+    
     const body = razorpay_order_id + '|' + razorpay_payment_id
 
     const expectedSignature = crypto
@@ -37,10 +34,53 @@ export const paymentVerification = async (req, res) => {
     if (isAuthentic) {
         res.json({
             success: true,
+            id:razorpay_payment_id,
         })
     } else {
         res.json({
             success: false,
         })
+    }
+}
+
+export const Refund = async (req, res) => {
+    try {
+        const {payment_id } = req.params;
+
+        const options = {
+            amount: Number(req.body.amount * 100),
+            speed: "normal",
+            // receipt: "Receipt no."+count,
+        }
+        const refund_order = await instance.payments.refund(payment_id, options);
+        
+        res.status(201).json({
+            success: true,
+            data: refund_order,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: "Internal server error"
+        });
+    }
+}
+
+export const find_refund = async(req,res) => {
+    try{
+        const {refund_id} = req.params;
+
+        const refund_details = await instance.refunds.fetch(refund_id);
+
+        res.status(200).json({
+            success:true,
+            data:refund_details,
+        })
+
+    } catch(error){
+        res.status(500).json({
+            success: false,
+            error: "Internal server error"
+        });
     }
 }
