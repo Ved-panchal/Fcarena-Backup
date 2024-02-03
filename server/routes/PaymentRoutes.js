4f37f3a299474e51d817ab7cb468e87017438258
Ved panchal <128056477+Ved-panchal@users.noreply.github.com>
2024-02-03 19:54:51 +0530

    Update PaymentRoutes.js

--- server/routes/PaymentRoutes.js
+++ server/routes/PaymentRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
 import {
+  Refund,
   checkout,
   paymentVerification,
+  find_refund,
 } from "../controllers/PaymentControllers.js";
 
 const router = express.Router();
@@ -9,5 +11,9 @@ const router = express.Router();
 router.route("/checkout").post(checkout);
 
 router.route("/paymentverification").post(paymentVerification);
+
+router.route("/refund/:payment_id").post(Refund);
+
+router.route("/refundDetails/:refund_id").get(find_refund);
 
 export default router;
