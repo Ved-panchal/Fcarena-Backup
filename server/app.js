d2e1cb3a1364ef28bee30dccb2013b8eccd78c8d
Ved panchal <128056477+Ved-panchal@users.noreply.github.com>
2024-02-07 23:36:55 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ config({ path: "./config/config.env" });
 
 export const app = express();
 
-app.use(cors({origin: "https://fcarenavadodara.in",credentials: true }));
+app.use(cors({origin: ["https://fcarenavadodara.in", "http://localhost:5173"],credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
