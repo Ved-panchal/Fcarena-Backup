784762dc4bf19d95fba01a2ef2aa180d158b0bc7
Ved panchal <128056477+Ved-panchal@users.noreply.github.com>
2024-02-09 00:07:07 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,20 +3,23 @@ import { config } from "dotenv";
 import paymentRoute from "./routes/PaymentRoutes.js";
 import cors from "cors";
 
-export const app = express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", paymentRoute);
-try{
-  app.get("/api/getkey", (req, res) =>
-  res.status(200).json({ key: process.env.RAZORPAY_API_KEY })
-);  
-} catch(err){
-  res.status(500).json({
-    success:false,
-    message:err.message,
-  })
-}
+
+app.get("/api/getkey", (req, res) => {
+  try {
+    res.status(200).json({ key: process.env.RAZORPAY_API_KEY });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+export default app;
