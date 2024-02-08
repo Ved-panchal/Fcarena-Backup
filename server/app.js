c6158e904e0428d3e955b6bd08cd20cb3409d595
Ved panchal <128056477+Ved-panchal@users.noreply.github.com>
2024-02-08 23:54:09 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -10,8 +10,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", paymentRoute);
-
-app.get("/api/getkey", (req, res) =>
+try{
+  app.get("/api/getkey", (req, res) =>
   res.status(200).json({ key: process.env.RAZORPAY_API_KEY })
-);
-
+);  
+} catch(err){
+  res.status(500).json({
+    success:false;
+    message:err.message;
+  })
+}
